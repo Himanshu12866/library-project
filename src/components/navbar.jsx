e680b2dd0b0c71b0a827f88ec7d665464f5166da
Himanshu12866 <manshu010m@gmail.com>
2024-09-26 19:13:17 +0530

    "Updated navbar component and bootstrap icon imports"

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,36 +7,36 @@ export default function NavBar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg  p-3 bg-dark bg-light">
-                <a className="navbar-brand text-light fw-bold"  href="kmn">Video Library</a>
-                <button className="btn btn-dark navbar-toggler" data-bs-toggle="collapse" data-bs-target="#list">
-                    <span className="bi bi-list"></span>
+                <a className="navbar-brand text-light fw-bold" href="kmn">Video Library</a>
+                <button className="navbar-toggler" data-bs-target="#list" data-bs-toggle="collapse" >
+                    <span className="bi bi-list text-light"></span>
                 </button>
-                <div className="navbar-collapse collapse d-lg-flex justify-lg-content-between" id="list">
+                <div className="navbar-collapse collapse d-lg-flex justify-content-lg-between" id="list">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                        <Link to="" className="nav-link text-light">Admin Login</Link>
+                            <Link to="" className="nav-link text-light">Admin Login</Link>
                         </li>
                         <li className="nav-item">
-                        <Link to="" className="nav-link text-light">User Login</Link>
+                            <Link to="" className="nav-link text-light">User Login</Link>
                         </li>
                         <li className="nav-item">
-                        <Link to="" className="nav-link text-light">Dashboard</Link>
+                            <Link to="" className="nav-link text-light">Dashboard</Link>
                         </li>
                     </ul>
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link  className="nav-link text-light">
-                                <span className="bi bi-person-fill"></span>
+                            <Link className="nav-link text-light">
+                                <span className="bi bi-person-fill fs-4 text-light"></span>
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link  className="nav-link text-light">
-                                <span className="bi bi-save"></span>
+                            <Link className="nav-link text-light">
+                                <span className="bi bi-cloud-plus-fill fs-4"></span>
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link  className="nav-link text-light">
-                                <span className="bi bi-person-fill"></span>
+                            <Link className="nav-link text-light">
+                                <span className="bi bi-box-arrow-right fs-4"></span>
                             </Link>
                         </li>
                     </ul>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import "../node_modules/bootstrap-icons/font/bootstrap-icons";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
