6b007b5236d75003d32f746cc9626c373bd6cf52
Himanshu12866 <manshu010m@gmail.com>
2024-09-26 19:01:42 +0530

    "Updated navbar component with new links and icons"

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,46 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-// eslint-disable-next-line no-unused-vars
-import { Button } from "@mui/material";
+
+
+import { Link } from "react-router-dom";
 
 
 export default function NavBar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg  p-3 bg-dark bg-light">
-                <a className="navbar-brand text-light fw-bold" >Video Library</a>
+                <a className="navbar-brand text-light fw-bold"  href="kmn">Video Library</a>
                 <button className="btn btn-dark navbar-toggler" data-bs-toggle="collapse" data-bs-target="#list">
                     <span className="bi bi-list"></span>
                 </button>
+                <div className="navbar-collapse collapse d-lg-flex justify-lg-content-between" id="list">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        <Link to="" className="nav-link text-light">Admin Login</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link to="" className="nav-link text-light">User Login</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link to="" className="nav-link text-light">Dashboard</Link>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link  className="nav-link text-light">
+                                <span className="bi bi-person-fill"></span>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link  className="nav-link text-light">
+                                <span className="bi bi-save"></span>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link  className="nav-link text-light">
+                                <span className="bi bi-person-fill"></span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>
     )
